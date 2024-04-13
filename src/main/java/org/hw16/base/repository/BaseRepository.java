12cd4997aac803b6a8e2c3230f0d2bc458c7e9ec
amirmhmdzdh <amir0mohammadzadeh@gmail.com>
2024-04-13 18:49:26 +0430

    add BaseRepository

---
+++ src/main/java/org/hw16/base/repository/BaseRepository.java
@@ -0,0 +1,19 @@
+package org.hw16.base.repository;
+
+import org.hw16.base.entity.BaseEntity;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Optional;
+
+public interface BaseRepository<T extends BaseEntity<ID>, ID extends Serializable> {
+
+    T saveOrUpdate(T entity);
+
+    Optional<T> findById(ID id);
+
+    void delete(T entity);
+
+    List<T> findAll();
+
+}
