fd433a34c7643eb52875d4d97189f8de9e82d322
amirmhmdzdh <amir0mohammadzadeh@gmail.com>
2024-04-21 20:55:52 +0430

    add method

---
+++ src/main/java/org/hw16/repository/SemesterRepository.java
@@ -0,0 +1,7 @@
+package org.hw16.repository;
+
+import org.hw16.base.repository.BaseRepository;
+import org.hw16.model.Semester;
+
+public interface SemesterRepository extends BaseRepository<Semester,Long> {
+}
