285bcdf1880eac4511a59a6d984c9e8fcda0865d
amirmhmdzdh <amir0mohammadzadeh@gmail.com>
2024-04-21 20:01:35 +0430

    add annotation

--- src/main/java/org/hw16/model/ReleasedCourse.java
+++ src/main/java/org/hw16/model/ReleasedCourse.java
@@ -3,24 +3,31 @@ package org.hw16.model;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-import lombok.ToString;
 import org.hw16.base.entity.BaseEntity;
 
 @Entity
 @Getter
 @Setter
-@ToString
 @Table(name = "released_course")
 public class ReleasedCourse extends BaseEntity<Long> {
 
-    @ManyToOne(cascade = {CascadeType.PERSIST,CascadeType.MERGE})
+    @ManyToOne
     private Teacher teacher;
 
-    @ManyToOne(cascade = {CascadeType.PERSIST,CascadeType.MERGE})
+    @ManyToOne
     @JoinColumn(name = "course_id")
     private Course course;
 
-    @ManyToOne(cascade = {CascadeType.PERSIST,CascadeType.MERGE})
+    @ManyToOne
     @JoinColumn(name = "semester_id")
     private Semester semester;
+
+    @Override
+    public String toString() {
+        return "ReleasedCourse{" +
+                "teacher=" + teacher.getLastName() + " " + teacher.getEmail() +
+                ", course=" + course +
+                ", semester=" + semester +
+                '}';
+    }
 }
