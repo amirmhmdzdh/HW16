38cdc9ab6a6011f4ebbb1c6872f3f8961cbff102
amirmhmdzdh <amir0mohammadzadeh@gmail.com>
2024-04-21 22:09:39 +0430

    add method

---
+++ src/main/java/org/hw16/repository/CourseRepository.java
@@ -0,0 +1,7 @@
+package org.hw16.repository;
+
+import org.hw16.base.repository.BaseRepository;
+import org.hw16.model.Course;
+
+public interface CourseRepository extends BaseRepository<Course, Long> {
+}
