5b97c57571e9ae5c42b6f55662aa76a5e99c16ba
amirmhmdzdh <amir0mohammadzadeh@gmail.com>
2024-04-21 20:57:00 +0430

    add method

---
+++ src/main/java/org/hw16/repository/ReleasedCourseRepository.java
@@ -0,0 +1,10 @@
+package org.hw16.repository;
+
+import org.hw16.base.repository.BaseRepository;
+import org.hw16.model.ReleasedCourse;
+
+import java.util.List;
+
+public interface ReleasedCourseRepository extends BaseRepository<ReleasedCourse, Long> {
+    List<ReleasedCourse> findEligibleReleasedCoursesForStudent(Long studentId);
+}
