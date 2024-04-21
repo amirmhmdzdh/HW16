135b4f6edf53f7488a9fbf88e02814da031fd3fc
amirmhmdzdh <amir0mohammadzadeh@gmail.com>
2024-04-21 20:31:54 +0430

    add method

---
+++ src/main/java/org/hw16/repository/TeacherRepository.java
@@ -0,0 +1,15 @@
+package org.hw16.repository;
+
+import org.hw16.base.repository.BaseRepository;
+import org.hw16.model.Teacher;
+
+import java.util.List;
+
+public interface TeacherRepository extends BaseRepository<Teacher, Long> {
+    Teacher findByNationalCodeAndPassword(String nationalCode, String password);
+
+    Teacher findByFirstnameAndLastname(String firstname, String lastname);
+
+    Teacher findByEmail(String email);
+
+}
