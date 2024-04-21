73edb5a09616de446d66c773d98a2f857d485304
amirmhmdzdh <amir0mohammadzadeh@gmail.com>
2024-04-21 20:09:29 +0430

    add annotation

--- src/main/java/org/hw16/model/StudentTakenCourse.java
+++ src/main/java/org/hw16/model/StudentTakenCourse.java
@@ -16,23 +16,39 @@ import javax.validation.constraints.DecimalMin;
 @ToString
 @Table(name = "student_taken_course")
 public class StudentTakenCourse extends BaseEntity<Long> {
-    @ManyToOne(cascade = {CascadeType.MERGE, CascadeType.PERSIST})
+    @ManyToOne
     @JoinColumn(name = "released_course_id")
     private ReleasedCourse releasedCourse;
 
-    @DecimalMin(value = "0.0", message = "Mark must be greater than or equal to 0.0")
-    @DecimalMax(value = "20.0", message = "Mark must be less than or equal to 10000.0")
+    @DecimalMin(value = "0.0", message = "The scoring floor is 0.0 points")
+    @DecimalMax(value = "20.0", message = "The scoring limit is 20 points")
     private Double mark;
 
     @Enumerated(EnumType.STRING)
     @Column(name = "course_state")
     private CourseState courseState;
 
-    @ManyToOne(cascade = {CascadeType.MERGE, CascadeType.PERSIST})
+    @ManyToOne
     private Student student;
 
-    private void setCourseState(){
-        if (this.mark < 10){
+    public StudentTakenCourse() {
+    }
+
+    public StudentTakenCourse(ReleasedCourse releasedCourse, Double mark, Student student) {
+        this.releasedCourse = releasedCourse;
+        this.mark = mark;
+        this.student = student;
+    }
+
+    public StudentTakenCourse(Long aLong, ReleasedCourse releasedCourse, Double mark, Student student) {
+        super(aLong);
+        this.releasedCourse = releasedCourse;
+        this.mark = mark;
+        this.student = student;
+    }
+
+    private void setCourseState() {
+        if (this.mark < 10) {
             this.courseState = CourseState.FAILED;
         } else
             this.courseState = CourseState.PASSED;
