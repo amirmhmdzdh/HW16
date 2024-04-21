7eca8c4b08e34d42193666362057c660d23f9c08
amirmhmdzdh <amir0mohammadzadeh@gmail.com>
2024-04-21 19:58:37 +0430

    add annotation

--- src/main/java/org/hw16/model/Course.java
+++ src/main/java/org/hw16/model/Course.java
@@ -17,23 +17,27 @@ public class Course extends BaseEntity<Long> {
 
     @NotBlank(message = "Course title is required")
     @Size(max = 100, message = "Course title must not exceed 100 characters")
-    @Pattern(regexp="^[a-zA-Z0-9]+$", message = "Course title must consist of letters and numbers only")
+    @Pattern(regexp = "^[a-zA-Z0-9]+$", message = "Course title must consist of letters and numbers only")
     private String title;
 
     @Min(value = 1, message = "Credits must be at least 1")
     @Max(value = 10, message = "Credits cannot exceed 10")
     private Integer credit;
 
-    @ManyToOne(cascade = {CascadeType.PERSIST,CascadeType.MERGE})
-    @JoinColumn(name = "course_department_id")
-    private Department courseDepartment;
 
     public Course() {
     }
 
-    public Course(String title, Integer credit, Department courseDepartment) {
+    public Course(String title, Integer credit) {
         this.title = title;
         this.credit = credit;
-        this.courseDepartment = courseDepartment;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "title='" + title + '\'' +
+                ", credit=" + credit +
+                '}';
     }
 }
