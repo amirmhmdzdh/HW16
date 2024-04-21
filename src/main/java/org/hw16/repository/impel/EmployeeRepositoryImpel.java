1c9e2d6b8982fac4c0a68eac8a0f7caa2563b52e
amirmhmdzdh <amir0mohammadzadeh@gmail.com>
2024-04-21 22:08:53 +0430

    add method

---
+++ src/main/java/org/hw16/repository/impel/EmployeeRepositoryImpel.java
@@ -0,0 +1,47 @@
+package org.hw16.repository.impel;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hw16.base.repository.BaseRepositoryImpel;
+import org.hw16.model.Employee;
+import org.hw16.repository.EmployeeRepository;
+
+public class EmployeeRepositoryImpel extends BaseRepositoryImpel<Employee, Long>
+        implements EmployeeRepository {
+    public EmployeeRepositoryImpel(SessionFactory sessionFactory) {
+        super(sessionFactory);
+    }
+
+    @Override
+    public Class<Employee> getEntityClass() {
+        return Employee.class;
+    }
+
+    @Override
+    public Employee findByNationalCodeAndPassword(String nationalCode, String password) {
+        Session session = sessionFactory.getCurrentSession();
+        String hql = "FROM Employee WHERE nationalCode = :nationalCode AND password = :password";
+        return session.createQuery(hql, Employee.class)
+                .setParameter("nationalCode", nationalCode)
+                .setParameter("password", password)
+                .uniqueResult();
+    }
+    @Override
+    public Employee findByFirstnameAndLastname(String firstname, String lastname) {
+        Session session = sessionFactory.getCurrentSession();
+        String hql = "FROM Employee WHERE firstName = :firstname AND lastName = :lastname";
+        return session.createQuery(hql, Employee.class)
+                .setParameter("firstname", firstname)
+                .setParameter("lastname", lastname)
+                .uniqueResult();
+    }
+
+    @Override
+    public Employee findByEmail(String email) {
+        Session session = sessionFactory.getCurrentSession();
+        String hql = "FROM Employee WHERE email = :email";
+        return session.createQuery(hql, Employee.class)
+                .setParameter("email", email)
+                .uniqueResult();
+    }
+}
