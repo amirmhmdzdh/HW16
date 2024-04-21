76e0d27e8dc514bb010808877ad22037149a6ea2
amirmhmdzdh <amir0mohammadzadeh@gmail.com>
2024-04-21 20:18:15 +0430

    add Method

--- src/main/java/org/hw16/base/service/BaseServiceImpel.java
+++ src/main/java/org/hw16/base/service/BaseServiceImpel.java
@@ -6,28 +6,36 @@ import org.hibernate.Transaction;
 import org.hw16.base.entity.BaseEntity;
 import org.hw16.base.repository.BaseRepository;
 import org.hw16.exception.NotFoundExeption;
+import org.hw16.utility.EntityValidator;
 
+import javax.validation.ConstraintViolation;
+import javax.validation.Validator;
 import java.io.Serializable;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 public class BaseServiceImpel<T extends BaseEntity<ID>,
         ID extends Serializable,
         R extends BaseRepository<T, ID>>
         implements BaseService<T, ID> {
 
-    private final R repository;
-    private final SessionFactory sessionFactory;
+    protected final R repository;
+    protected final SessionFactory sessionFactory;
+    private final Validator validator;
 
     public BaseServiceImpel(R repository, SessionFactory sessionFactory) {
         this.repository = repository;
         this.sessionFactory = sessionFactory;
+        this.validator = EntityValidator.validator;
     }
 
     @Override
     public T saveOrUpdate(T entity) {
-
         Transaction transaction = null;
+
+        if (!isValid(entity))
+            return null;
         try (Session session = sessionFactory.getCurrentSession()) {
             transaction = session.beginTransaction();
 
@@ -60,26 +68,37 @@ public class BaseServiceImpel<T extends BaseEntity<ID>,
 
     @Override
     public void deleteById(ID id) {
-        try (Session session = sessionFactory.getCurrentSession()){
+        try (Session session = sessionFactory.getCurrentSession()) {
             session.beginTransaction();
             Optional<T> findEntity = repository.findById(id);
             findEntity.orElseThrow(() -> new NotFoundExeption(String.format("Entity with id : %s not found", id)));
             repository.delete(findEntity.get());
             session.getTransaction().commit();
+        } catch (Exception ignored) {
         }
-        catch (Exception ignored){}
     }
+
     @Override
-    public List<T> findAll() {
-        try (Session session = sessionFactory.getCurrentSession()){
+    public List<T> showAll() {
+        try (Session session = sessionFactory.getCurrentSession()) {
             session.beginTransaction();
-            List<T> listAll = repository.findAll();
+            List<T> showAll = repository.showAll();
             session.getTransaction().commit();
             session.close();
-            return listAll;
-        }
-        catch (Exception ignored){
+            return showAll;
+        } catch (Exception ignored) {
             return null;
         }
+    }
+
+    @Override
+    public boolean isValid(T t) {
+        Set<ConstraintViolation<T>> violations = validator.validate(t);
+        if (!violations.isEmpty()) {
+            for (ConstraintViolation<T> p : violations)
+                System.out.println(p.getMessage());
+            return false;
+        }
+        return true;
     }
 }
