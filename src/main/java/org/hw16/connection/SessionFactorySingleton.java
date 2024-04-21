1089f4a385c21f1f20b8a03f5f64f897426fbd07
amirmhmdzdh <amir0mohammadzadeh@gmail.com>
2024-04-21 20:18:47 +0430

    add Method

--- src/main/java/org/hw16/connection/SessionFactorySingleton.java
+++ src/main/java/org/hw16/connection/SessionFactorySingleton.java
@@ -6,35 +6,29 @@ import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hw16.model.*;
 
 public class SessionFactorySingleton {
-
     private SessionFactorySingleton() {
     }
 
-    private static class LazyHolder {
-        static SessionFactory INSTANCE;
-
-        static {
-            var registry = new StandardServiceRegistryBuilder()
-                    .configure()
-                    .build();
-
-            INSTANCE = new MetadataSources(registry)
-                    .addAnnotatedClass(Person.class)
-                    .addAnnotatedClass(Student.class)
-                    .addAnnotatedClass(Teacher.class)
-                    .addAnnotatedClass(Employee.class)
-                    .addAnnotatedClass(Course.class)
-                    .addAnnotatedClass(Department.class)
-                    .addAnnotatedClass(ReleasedCourse.class)
-                    .addAnnotatedClass(Semester.class)
-                    .addAnnotatedClass(StudentTakenCourse.class)
-
-                    .buildMetadata()
-                    .buildSessionFactory();
-        }
+    private static class SessionFactoryHelper {
+        static org.hibernate.service.ServiceRegistry registry =
+                new StandardServiceRegistryBuilder()
+                        .configure()
+                        .build();
+        private static final SessionFactory INSTANCE =
+                new MetadataSources(registry)
+                        .addAnnotatedClass(Person.class)
+                        .addAnnotatedClass(Student.class)
+                        .addAnnotatedClass(Teacher.class)
+                        .addAnnotatedClass(Employee.class)
+                        .addAnnotatedClass(Course.class)
+                        .addAnnotatedClass(ReleasedCourse.class)
+                        .addAnnotatedClass(Semester.class)
+                        .addAnnotatedClass(StudentTakenCourse.class)
+                        .buildMetadata()
+                        .buildSessionFactory();
     }
 
     public static SessionFactory getInstance() {
-        return LazyHolder.INSTANCE;
+        return SessionFactoryHelper.INSTANCE;
     }
 }
