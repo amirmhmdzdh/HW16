5a9d6d37d0d050d02022c92522f13768a2052b95
amirmhmdzdh <amir0mohammadzadeh@gmail.com>
2024-04-21 19:40:18 +0430

    add annotation

--- src/main/java/org/hw16/model/Person.java
+++ src/main/java/org/hw16/model/Person.java
@@ -7,7 +7,9 @@ import lombok.ToString;
 import org.hw16.base.entity.BaseEntity;
 
 import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 
 @Setter
@@ -18,37 +20,44 @@ import javax.validation.constraints.Pattern;
 @DiscriminatorColumn(discriminatorType = DiscriminatorType.STRING)
 public class Person extends BaseEntity<Long> {
 
-    @Pattern(regexp="^[a-zA-Z]+$", message = "FirstName must consist of letters only")
-    private String firstname;
+    @Pattern(regexp = "^[a-zA-Z]+$", message = "FirstName must consist of letters only")
+    @Size(max = 50, min = 3, message = "Invalid name. Size should be between 3 to 20.")
+    @NotEmpty(message = "Please Enter your name.")
+    private String firstName;
 
-    @Pattern(regexp="^[a-zA-Z]+$", message = "LastName must consist of letters only")
-    private String lastname;
+    @Pattern(regexp = "^[a-zA-Z]+$", message = "LastName must consist of letters only")
+    @Size(max = 50, min = 3, message = "Invalid name. Size should be between 3 to 20.")
+    @NotEmpty(message = "Please Enter your lastName.")
+    private String lastName;
 
     @Pattern(regexp = "^\\d{10}$", message = "nationalCod must consist of numbers only")
+    @NotEmpty(message = "Please Enter your NationalCode.")
     private String nationalCode;
 
     @Pattern(regexp = "^[a-zA-Z0-9]+$", message = "Password must consist of letters and numbers only")
+    @NotEmpty(message = "Please Enter your Password.")
     private String password;
 
     @Email(message = "Email should be valid")
+    @NotEmpty(message = "Please Enter your Email.")
     private String email;
 
 
     public Person() {
     }
 
-    public Person(Long aLong, String firstname, String lastname, String nationalCode, String password, String email) {
+    public Person(Long aLong, String firstName, String lastName, String nationalCode, String password, String email) {
         super(aLong);
-        this.firstname = firstname;
-        this.lastname = lastname;
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.nationalCode = nationalCode;
         this.password = password;
         this.email = email;
     }
 
-    public Person(String firstname, String lastname, String nationalCode, String password, String email) {
-        this.firstname = firstname;
-        this.lastname = lastname;
+    public Person(String firstName, String lastName, String nationalCode, String password, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.nationalCode = nationalCode;
         this.password = password;
         this.email = email;
