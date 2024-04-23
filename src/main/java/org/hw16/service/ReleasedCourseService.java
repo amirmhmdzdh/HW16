be6801358260fbee344d24d1895a6c5a2af9cc98
amirmhmdzdh <amir0mohammadzadeh@gmail.com>
2024-04-23 11:37:20 +0430

    Edite Class

--- src/main/java/org/hw16/service/ReleasedCourseService.java
+++ src/main/java/org/hw16/service/ReleasedCourseService.java
@@ -3,8 +3,5 @@ package org.hw16.service;
 import org.hw16.base.service.BaseService;
 import org.hw16.model.ReleasedCourse;
 
-import java.util.List;
-
-public interface ReleasedCourseService extends BaseService<ReleasedCourse,Long> {
-    List<ReleasedCourse> findEligibleReleasedCoursesForStudent(Long studentId);
+public interface ReleasedCourseService extends BaseService<ReleasedCourse, Long> {
 }
