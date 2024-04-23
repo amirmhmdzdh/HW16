ba4342e4996c14bb14ca58112546cb693d9a62bd
amirmhmdzdh <amir0mohammadzadeh@gmail.com>
2024-04-23 11:26:35 +0430

    Edite Class

--- src/main/java/org/hw16/service/EmployeeService.java
+++ src/main/java/org/hw16/service/EmployeeService.java
@@ -3,8 +3,9 @@ package org.hw16.service;
 import org.hw16.base.service.BaseService;
 import org.hw16.model.Employee;
 
-public interface EmployeeService extends BaseService<Employee,Long> {
+public interface EmployeeService extends BaseService<Employee, Long> {
     Employee signIn(String nationalCode, String password);
-    Employee signUp(String firstname, String lastname, String username, String password, String email, Long salary);
+
+    Employee signUp(Employee employee);
 
 }
