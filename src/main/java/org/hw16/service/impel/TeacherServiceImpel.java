3c9fe186eb3dc233282d9a05f826647b582adc10
amirmhmdzdh <amir0mohammadzadeh@gmail.com>
2024-04-23 11:41:59 +0430

    Edite Class

--- src/main/java/org/hw16/service/impel/TeacherServiceImpel.java
+++ src/main/java/org/hw16/service/impel/TeacherServiceImpel.java
@@ -7,7 +7,6 @@ import org.hibernate.Transaction;
 import org.hw16.base.service.BaseServiceImpel;
 import org.hw16.exception.SignInException;
 import org.hw16.model.Teacher;
-import org.hw16.model.enums.TeacherLevel;
 import org.hw16.repository.TeacherRepository;
 import org.hw16.service.TeacherService;
 
@@ -20,10 +19,8 @@ public class TeacherServiceImpel
 
     public Teacher signIn(String nationalCode, String password) {
         Transaction transaction = null;
-
         try (Session session = sessionFactory.getCurrentSession()) {
             transaction = session.beginTransaction();
-
             Teacher teacher = repository.findByNationalCodeAndPassword(nationalCode, password);
             if (teacher != null) {
                 System.out.println("Signed in successfully!");
@@ -32,34 +29,19 @@ public class TeacherServiceImpel
             } else {
                 transaction.rollback();
                 throw new SignInException("Invalid national code or password. Failed.");
-
             }
         }
     }
 
     @Override
-    public Teacher signUp(String firstname, String lastname, String nationalCode, String password, String email, Long baseSalary, TeacherLevel teacherLevel) {
+    public Teacher signUp(Teacher teacher) {
         Transaction transaction = null;
         try (Session session = sessionFactory.getCurrentSession()) {
             transaction = session.beginTransaction();
-
-            if (repository.findByNationalCodeAndPassword(nationalCode, password) != null ||
-                    repository.findByFirstnameAndLastname(firstname, lastname) != null ||
-                    repository.findByEmail(email) != null) {
-                throw new SignInException("User with the same username, name, or email already exists");
-            }
-
-            Teacher newTeacher = new Teacher();
-            newTeacher.setFirstName(firstname);
-            newTeacher.setLastName(lastname);
-            newTeacher.setNationalCode(nationalCode);
-            newTeacher.setPassword(password);
-            newTeacher.setEmail(email);
-            newTeacher.setTeacherLevel(teacherLevel);
-            newTeacher.setBaseSalary(baseSalary);
+            Teacher saveOrUpdate = repository.saveOrUpdate(teacher);
             transaction.commit();
-
-            return saveOrUpdate(newTeacher);
+            session.close();
+            return saveOrUpdate;
         } catch (HibernateException e) {
             if (transaction != null) {
                 transaction.rollback();
