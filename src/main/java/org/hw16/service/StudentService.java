d0e5d29077e5179bfb4fd5bb0d42e1536ad7b62e
amirmhmdzdh <amir0mohammadzadeh@gmail.com>
2024-04-23 11:42:30 +0430

    Edite Class

--- src/main/java/org/hw16/service/StudentService.java
+++ src/main/java/org/hw16/service/StudentService.java
@@ -3,10 +3,9 @@ package org.hw16.service;
 import org.hw16.base.service.BaseService;
 import org.hw16.model.Student;
 
-import java.util.List;
 
 public interface StudentService extends BaseService<Student, Long> {
     Student signIn(String nationalCode, String password);
 
-    Student signUp(String firstname, String lastname, String nationalCode, String password, String email);
+    Student signUp(Student student);
 }
--- src/main/java/org/hw16/service/TeacherService.java
+++ src/main/java/org/hw16/service/TeacherService.java
@@ -2,13 +2,11 @@ package org.hw16.service;
 
 import org.hw16.base.service.BaseService;
 import org.hw16.model.Teacher;
-import org.hw16.model.enums.TeacherLevel;
 
 public interface TeacherService extends BaseService<Teacher, Long> {
     Teacher signIn(String username, String password);
 
-    Teacher signUp(String firstname, String lastname, String username, String password, String email,
-                   Long baseSalary, TeacherLevel teacherLevel);
+    Teacher signUp(Teacher teacher);
 
 
 
